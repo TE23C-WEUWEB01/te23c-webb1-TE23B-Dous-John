54df30ee013588f10254c6e720a45b1a723507e5
John Dous <john.dous@elev.ga.ntig.se>
2025-03-14 09:45:40 +0100

    Tränat på inmatning med prompt och skriva i webbfönstret

---
+++ kapitel-8/intro-js/script.js
@@ -0,0 +1,17 @@
+// var tal1 = 12;
+// var tal2 = 34;
+// var summa = tal1 + tal2;
+// alert(tal1 + " + " + tal2 + " = " + summa);
+
+var förnamn = "John";
+var efternamn = "Dous";
+var ålder = 18;
+var namn = förnamn + " " + efternamn;
+// var meddelande = namn + " är " + ålder + " år gammel "
+// alert(meddelande);
+
+var hobby = "fotboll"
+var kostnad = 1000;
+var meddelande = förnamn + " " + efternamn + " gillar " + hobby + "\noch det kostar " + kostnad + " per år";
+// Tex: John Dous gillar fotboll.
+alert(meddelande);
